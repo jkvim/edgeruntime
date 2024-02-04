28d500d19ebbc5af0b9ce732b1f07da0cf1dbf64
jkvim <jkvim@outlook.com>
2024-02-04 15:12:46 +0800

    update footer with external link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { FullScreenRoutes } from "@/constants/route-config";
+import Link from "next/link";
 
 export const Footer = () => {
   const {route} = useRouter();
@@ -8,10 +9,10 @@ export const Footer = () => {
   }
 
   return (
-    <footer
-      className="footer footer-center p-8 text-base-content gap-y-4 bg-base-100 mt-20">
-      <aside>
+    <footer className="footer footer-center p-8 text-base-content gap-y-4 bg-base-100 mt-20">
+      <aside className="flex">
         <p>Copyright © 2024 - Edgeruntime.ai</p>
+        <Link href="https://text2img.xyz/">Text2Img</Link>
       </aside>
     </footer>
   );
